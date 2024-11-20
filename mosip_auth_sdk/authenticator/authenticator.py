bcb6051072891b08a590b8cb0b2d2a40bd9bebe9
abdulbathish <arbathish@noreply.github.com>
2024-11-20 13:15:22 +0530

    MosipAuthenticator: expose 2 methods (kyc, auth) and provide docs for them

--- mosip_auth_sdk/authenticator/authenticator.py
+++ mosip_auth_sdk/authenticator/authenticator.py
@@ -5,39 +5,89 @@ import sys
 import traceback
 import urllib
 from datetime import datetime
-from typing import Literal, Optional
+from typing import Literal, Optional, Dict
 from .model import MOSIPAuthRequest, DemographicsModel, MOSIPEncryptAuthRequest
 from .utils import CryptoUtility, RestUtility
 from .exceptions import AuthenticatorException, Errors
 
-type AuthController = Literal['ekyc', 'auth']
-type AuthControllerWay = Literal['demo', 'kyc', 'otp']
+type AuthController = Literal['kyc', 'auth']
 
 class MOSIPAuthenticator:
-    def __init__(self, config_obj, logger=None, **kwargs):
+    """
+Wrapper for the Mosip Authentication Service.
+
+    MOSIP provides two authentication controllers:
+    1. kyc-auth-controller:
+       - Reference: https://mosip.github.io/documentation/1.2.0/authentication-service.html#tag/kyc-auth-controller
+    2. auth-controller:
+       - Reference: https://mosip.github.io/documentation/1.2.0/authentication-service.html#operation/authenticateIndividual
+
+    These methods are exposed via the `kyc` and `auth` methods respectively.
+
+    Methods:
+        kyc(vid: str, demographic_data: DemographicsModel, otp_value: str = None, biometrics: list[BiometricModel] = None) -> Response:
+            Wrapper for the kyc-auth-controller
+
+        auth(vid: str, demographic_data: DemographicsModel, otp_value: Optional[str] = None, biometrics: Optional[List[BiometricModel]] = None) -> Response:
+            Wrapper for the auth-controller
+
+    Common Parameters for `auth`, `kyc`:
+        vid (str): The unique VID  of the individual to authenticate.
+        demographic_data (DemographicsModel): The demographic data for the individual.
+        otp_value (Optional[str]): The One-Time Password (OTP) value, if applicable. Default is None.
+        biometrics (Optional[List[BiometricModel]]): A list of biometric data models for the individual, if applicable. Default is None.
+
+    Example:
+    --------
+    ```python
+    from mosip_auth_sdk import MOSIPAuthenticator
+    from mosip_auth_sdk.models import DemographicsModel, BiometricModel
+
+    authenticator = MOSIPAuthenticator(config={
+        # Your configuration settings go here.
+        # Refer to authenticator-config.toml for the required values.
+    })
+
+    # Refer the DemographicsModel, BiometricModel documentation to know
+    # the exact arguments to be passed in there
+    demographics_data = DemographicsModel()
+    biometrics = [BiometricModel(), BiometricModel()]
+
+    # Make a KYC request
+    response = authenticator.kyc(
+        vid='<some_vid>',
+        demographic_data=demographics_data,
+        otp_value='323',  # Optional
+        biometrics=biometrics,  # Optional
+    )
+    ```
+
+    Attributes:
+        config (dict): Configuration settings for the authenticator.
+    """
+    def __init__(self, **, config, logger=None):
         if not logger:
-            self.logger = self._init_logger(config_obj.logging.log_file_path)
+            self.logger = self._init_logger(config.logging.log_file_path)
         else:
             self.logger = logger
 
-        self.auth_rest_util = RestUtility(config_obj.mosip_auth_server.ida_auth_url, config_obj.mosip_auth.authorization_header_constant)
-        self.crypto_util = CryptoUtility(config_obj.crypto_encrypt, config_obj.crypto_signature)
+        self.auth_rest_util = RestUtility(config.mosip_auth_server.ida_auth_url, config.mosip_auth.authorization_header_constant)
+        self.crypto_util = CryptoUtility(config.crypto_encrypt, config.crypto_signature)
 
-        self.auth_domain_scheme = config_obj.mosip_auth_server.ida_auth_domain_uri
+        self.auth_domain_scheme = config.mosip_auth_server.ida_auth_domain_uri
 
-        self.partner_misp_lk =  str(config_obj.mosip_auth.partner_misp_lk)
-        self.partner_id = str(config_obj.mosip_auth.partner_id)
-        self.partner_apikey = str(config_obj.mosip_auth.partner_apikey)
+        self.partner_misp_lk =  str(config.mosip_auth.partner_misp_lk)
+        self.partner_id = str(config.mosip_auth.partner_id)
+        self.partner_apikey = str(config.mosip_auth.partner_apikey)
 
-        self.ida_auth_version = config_obj.mosip_auth.ida_auth_version
-        self.ida_auth_request_id_by_controller = {
-            'demo': config_obj.mosip_auth.ida_auth_request_demo_id,
-            'otp': config_obj.mosip_auth.ida_auth_request_otp_id,
-            'kyc': config_obj.mosip_auth.ida_auth_request_kyc_id,
+        self.ida_auth_version = config.mosip_auth.ida_auth_version
+        self.ida_auth_request_id_by_controller: Dict[AuthController, str]  = {
+            'auth': config.mosip_auth.ida_auth_request_demo_id,
+            'kyc': config.mosip_auth.ida_auth_request_kyc_id,
         }
-        self.ida_auth_env = config_obj.mosip_auth.ida_auth_env
-        self.timestamp_format = config_obj.mosip_auth.timestamp_format
-        self.authorization_header_constant = config_obj.mosip_auth.authorization_header_constant
+        self.ida_auth_env = config.mosip_auth.ida_auth_env
+        self.timestamp_format = config.mosip_auth.timestamp_format
+        self.authorization_header_constant = config.mosip_auth.authorization_header_constant
 
     @staticmethod
     def _init_logger(filename):
@@ -74,6 +124,7 @@ class MOSIPAuthenticator:
             metadata = {},
             thumbprint = self.crypto_util.enc_cert_thumbprint,
             individualId = individual_id,
+            individualIdType = 'VID',
             transactionID = transaction_id,
             requestTime = timestamp_str,
             request = '',
@@ -81,19 +132,48 @@ class MOSIPAuthenticator:
             requestHMAC = '',
         )
 
-    def authenticate(
+    def kyc(self, **,
+            vid,
+            demographic_data: DemographicsModel,
+            otp_value: Optional[str]=None,
+            biometrics: Optional[List[BiometricModel]]=None
+            ):
+        return self.__authenticate(
+            controller='kyc'
+            vid=vid,
+            demographic_data=demographic_data,
+            otp_value=otp_value,
+            biometrics=biometrics
+        )
+
+    def auth(self, **,
+            vid,
+            demographic_data: DemographicsModel,
+            otp_value: Optional[str]=None,
+            biometrics: Optional[List[BiometricModel]]=None
+            ):
+        return self.__authenticate(
+            controller='',
+            vid=vid,
+            demographic_data=demographic_data,
+            otp_value=otp_value,
+            biometrics=biometrics
+        )
+
+    def __authenticate(
             self,
             **,
             controller: AuthController,
             vid: str,
             demographic_data: DemographicsModel,
             otp_value: Optional[str]=None,
-            biometrics: Optional[List[BiometricModel]]=None
+            biometrics: Optional[List[BiometricModel]]=[]
     ):
         '''
         '''
         self.logger.info('Received Auth Request for demographic.')
         auth_request = self._get_default_auth_request(
+            controller,
             individual_id=vid,
         )
         auth_request.requestedAuth.demo = True
@@ -101,8 +181,9 @@ class MOSIPAuthenticator:
         auth_request.requestedAuth.bio = bool(biometrics)
         request = MOSIPEncryptAuthRequest(
             timestamp = auth_request.requestTime,
-            biometrics = [],
+            biometrics = biometrics,
             demographics = demographic_data,
+            otp = ,
         )
         try:
             signature_header = {'Signature': self.crypto_util.sign_auth_request_data(full_request_json)}
@@ -111,14 +192,13 @@ class MOSIPAuthenticator:
             path_params = '/'.join(
                 map(
                     urllib.parse.quote,
-                    ('kyc' if controller == 'ekyc' else 'auth',
-                     self.partner_misp_lk, self.partner_id, self.partner_apikey)
+                    (controller, self.partner_misp_lk, self.partner_id, self.partner_apikey)
                 )
             )
             full_request_json = auth_request.json()
             response = self.auth_rest_util.post_request(path_params=path_params, data=full_request_json, additional_headers=signature_header)
             self.logger.info('Auth Request for Demographic Completed.')
-            return response.text
+            return response
         except:
             exp = traceback.format_exc()
             self.logger.error('Error Processing Auth Request. Error Message: {}'.format(exp))

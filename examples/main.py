410f4afc7ba9429769b3d23671d54891151473d0
abdulbathish <arbathish@noreply.github.com>
2024-11-28 01:55:08 +0530

    put in sample example code

---
+++ examples/main.py
@@ -0,0 +1,29 @@
+from mosip_auth_sdk import MOSIPAuthenticator
+from mosip_auth_sdk.models import DemographicsModel, BiometricModel
+from dynaconf import Dynaconf
+
+config = Dynaconf(
+    settings_files=['./tests/authenticator-config.toml'],
+    environments=False,
+)
+
+authenticator = MOSIPAuthenticator(config=config)
+demographics_data = DemographicsModel(
+    name=[{'language': 'en', 'value': 'Bathish'}],
+    gender=[{'language': 'en', 'value': 'Male'}],
+    dob="2002-12-01",
+    phoneNumber="9876543210",
+    emailId="arbathish@noreply.github.com",
+    fullAddress=[{'language': 'en', 'value': '123 Main St'}]
+)
+response = authenticator.kyc(
+    vid='4370296312658178',
+    demographic_data=demographics_data,
+)
+resp_body = response.json()
+errors = resp_body.get('errors') or []
+if errors:
+    for error in errors:
+        print(error.get('errorCode'), ':', error.get('errorMessage'))
+
+print(response.status_code)
